98918b593ee9683250593c7540047b6eb3912731
Camille Magnette <122868730+CamilleMagnette@users.noreply.github.com>
2023-05-02 16:02:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ liste_genres = pd.read_pickle("liste_genres.pkl.gz")
 liste_acteurs = pd.read_pickle("liste_acteurs.pkl.gz")
 liste_annees = pd.read_pickle("liste_annees.pkl.gz")
 
-
 # CONFIGURER LA PAGE
 st.set_page_config(
     page_title="Recommandation de Film App",
@@ -27,17 +26,19 @@ st.header("Dis moi quel film tu aimes et je t'en ferai aimer d'autres")
 
 # MULTI SELECT BOX : first argument takes the box title, second argument takes the options to show
 
-list_film_deroulante = ["Tape le film que tu aimes"] + list(liste_films["primaryTitle"])
+list_film_deroulante_films = ["Tape le film que tu aimes"] + list(liste_films["primaryTitle"])
+list_film_deroulante_acteurs = [""] + list(liste_acteurs["primaryName"])
 
+                                      
 with st.form("form 4"):
         col1, col2, col3, col4 = st.columns(4)
         with col1 :
-            films = st.selectbox("Films : ", list_film_deroulante)
+            films = st.selectbox("Films : ", list_film_deroulante_films)
             st.write("Tu as choisis : ", films, ". Bon choix ;)")
         with col2 :
             genres = st.multiselect("Genres : ", liste_genres)
         with col3 :
-            acteurs = st.selectbox("Acteurs : ", liste_acteurs)
+            acteurs = st.selectbox("Acteurs : ", list_film_deroulante_acteurs)
         with col4 :
             start_year, end_year = st.select_slider('Années',
                                     options=liste_annees["startYear"],
@@ -45,8 +46,8 @@ with st.form("form 4"):
             st.write('Vous avez choisi les années entre', start_year, 'et', end_year)
         submit : st.form_submit_button("Soumettre")
  
-if submit : 
-    st.write("Tu as choisi le film : ", films, " , le genre : ", genres ,"entre les années", start_year ,"et", end_year) 
+#if submit : 
+#    st.write("Tu as choisi le film : ", films, " , le genre : ", genres ,"entre les années", start_year ,"et", end_year) 
 
 # SOUS TITRE
 st.subheader("Bon visionnage !")
